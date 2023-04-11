3eb5ca17dc3038f0cb1f3db9c3667d3976b0027f
Mohamed-Ashraf273 <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2023-04-11 16:26:47 +0200

    Update scheduler.h

--- DS project/scheduler.h
+++ DS project/scheduler.h
@@ -1,1 +1,16 @@
 #pragma once
+#include"Processor_FCFS"
+#include"Processor_SJF"
+#include"Processor_RR"
+#include"Process.h"
+#include"Processor.h"
+#include"LinkedList.h"
+#include"Linkedqueue.h"
+#include"Stream.h"
+class scheduler:public Stream{
+LinkedList<Processor*> Processors;
+LinkedQueue<Process> Processes;
+int FCFS_no, SJF_no, RR_no,Process_no;
+public:
+scheduler();
+}
