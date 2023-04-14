78dff4fd97bbe83e3f7634331ea56b5210205635
Mohamed-Ashraf273 <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2023-04-14 21:52:55 +0200

    Update scheduler.h

--- DS project/scheduler.h
+++ DS project/scheduler.h
@@ -1,17 +1,25 @@
 #pragma once
 #include"Processor_FCFS.h"
-#include"Processor_SJF.h"
 #include"Processor_RR.h"
+#include"Processor_SJF.h"
 #include"Process.h"
 #include"Processor.h"
 #include"LinkedList.h"
 #include"Linkedqueue.h"
 #include"Stream.h"
-class scheduler:public Stream{
-LinkedList<Processor*> Processors;
-LinkedQueue<Process> NEW_LIST;
-Stream IO;
-int FCFS_no, SJF_no, RR_no,Processes_no;
+class scheduler :public Stream {
+	LinkedList<Processor*> Processors; // both linkedlist or queue can be used (linkedlist more flexible) 
+	LinkedQueue<Process*> NEW_LIST; // queue is used as it can maintain the processes that have been created but not yet arrived.
+	LinkedQueue<Process*> BLK_LIST; // The blocked list can be implemented using a queue The processes that are waiting for I/O resources can be added to the end of the queue and when the resources become available they can be moved to the ready queue
+	LinkedList<Process*> TRM_LIST; // The terminated list can be implemented using queue The processes that have completed their execution can be added to the end of the queue
+	Node<Processor*>* Ctrl_Processors; // pointer to control Processors list
+	Stream IO;
+	int FCFS_no, SJF_no, RR_no, Processes_no, Time_Step;
 public:
-scheduler();
-}
+	scheduler();
+	void AddToRdy(Process* p);
+	void simulate_system();// make the program ready to run
+	//void NewToRdy(Process* p);// returns the processor with the least CT (in phase 2)
+	void update_TimeStep();// inc timestep by 1
+	int GenerateNo();// generates a number between 1:100 for a process in run list
+};
