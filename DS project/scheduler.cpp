a5a790a2c5d90699aed5f3c6e55f8a9151130ad0
Mohamed-Ashraf273 <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2023-04-14 21:51:35 +0200

    Update scheduler.cpp

--- DS project/scheduler.cpp
+++ DS project/scheduler.cpp
@@ -1,24 +1,55 @@
 #include<iostream>
 #include"scheduler.h"
 using namespace std;
+template<class T>
+class Node {
+	T item;
+	Node* next;
+public:
+	Node() {};
+	Node(T newItem); //non-default constructor
+	void setItem(T a)
+	{
+		item = a;
+	}
+	T getItem() const
+	{
+		if (this == NULL)
+		{
+			return 0;
+		}
+		return item;
+	}
+	void setNext(Node<T>* n)
+	{
+		next = n;
+	}
+	Node<T>* getNext() const
+	{
+		return next;
+	}
+};
+
 scheduler::scheduler()
 {
+	Time_Step = 0;
+	Ctrl_Processors = Processors.gethead();
 	IO.get_counts(FCFS_no, SJF_no, RR_no, Processes_no);
 	// we will make one list of processors divided to three parts first part for FCFS, second for SJF and the third for RR
 	for (int i = 0; i < FCFS_no; i++)
 	{
 		Processor_FCFS* P = new Processor_FCFS;
-		ProcessorsList.InsertEnd(P);
+		Processors.InsertEnd(P);
 	}
 	for (int i = 0; i < SJF_no; i++)
 	{
 		Processor_SJF* P = new Processor_SJF;
-		ProcessorsList.InsertEnd(P);
+		Processors.InsertEnd(P);
 	}
 	for (int i = 0; i < RR_no; i++)
 	{
 		Processor_RR* P = new Processor_RR;
-		ProcessorsList.InsertEnd(P);
+		Processors.InsertEnd(P);
 	}
 	// fill the processes list
 	for (int i = 0; i < Processes_no; i++)
@@ -26,8 +57,131 @@ scheduler::scheduler()
 		int pid,no_IO, at, rt, ct, tt;
 		int* IO_r, int* IO_d;
 		IO.LoadProcesses(pid,no_IO,at,rt,ct,IO_r,IO_d);
-		Process p;
-		p.AddProcess(pid, no_IO, at, rt, ct, IO_r, IO_d);// please update process class as mentioned in the pdf so I can continue // updated
-		NEW_LIST.enqueue(p);
+		Process* p = new Process;
+		p->AddProcess(pid, no_IO, at, rt, ct, IO_r, IO_d);
 	}
+}
+
+
+
+
+// this function will be used in phase 2
+// 
+// insert a process to the processor with the least CT
+//void scheduler::NewToRdy(Process* p) 
+//{
+//	Node<Processor*>* ptr = Processors.gethead();
+//	int min_CT = ptr->getItem()->ExpectedFinishTime();
+//	ptr = ptr->getNext();
+//	while (ptr)
+//	{
+//		if (ptr->getItem()->ExpectedFinishTime() < min_CT)
+//			min_CT = ptr->getItem()->ExpectedFinishTime();
+//		ptr = ptr->getNext();
+//	}
+//	ptr = Processors.gethead();
+//	while (ptr)
+//	{
+//		if (ptr->getItem()->ExpectedFinishTime() == min_CT)
+//		{
+//			ptr->getItem()->AddToList(p);
+//		}
+//	}
+//}
+ 
+
+
+
+// we will complete filling the rdy lists
+void scheduler::simulate_system()
+{
+	int NO_Generated;// number generated for a run process
+	Process* p;
+	Process* p1;// temporary pointer to move from blk to rdy
+	Process* p2;// temporary pointer to a process from rdy list
+	Process* Run_P;// pointer to get the run process in a processor
+	Node<Processor*>* Pr_ptr1=Processors.gethead();// a pointer to processors list
+	Node<Processor*>* Pr_ptr2 = Processors.gethead();// a pointer to processors list
+	Node<Processor*>* Pr_ptr3 = Processors.gethead();// a pointer to processors list
+
+	NEW_LIST.peek(p);
+	// system here is working with checking the new list as its not empty yet
+	while (TRM_LIST.getcount() != Processes_no)// stop when all process move to trm list
+	{
+		// in each timestep we check:
+		// 1- processes with this time step will transfer them to the rdy list. Note: we won't make any balance in this phase
+		if (!NEW_LIST.isEmpty())
+		{
+			while (p->get_AT() == Time_Step)
+			{
+				NEW_LIST.dequeue(p);
+				AddToRdy(p);
+				NEW_LIST.peek(p);
+			}
+		}
+		// 2- Check the RDY lists and move one process from each RDY list to RUN state of its processor. This is done if the processor is IDLE. If the processor is BUSY the process should wait in the RDY list.
+		while (Pr_ptr1)
+		{
+			if (Pr_ptr1->getItem()->IsIdle())
+			{
+				Pr_ptr1->getItem()->RunProcess();// dont forget to make the process run state to be true
+			    // note : as shown in the project document that when a process move to run state it won't be in the ready list anymore
+			}
+			Pr_ptr1 = Pr_ptr1->getNext();
+		}
+        Pr_ptr1 = Processors.gethead();
+		// 3- For each process in RUN state, Generate a random number from 1 to 100.
+		while (Pr_ptr2)
+		{
+			if (!Pr_ptr2->getItem()->IsIdle())// if there is a process in the run state
+			{
+				Run_P = Pr_ptr2->getItem()->GetRunProcess();// get thr run process
+				NO_Generated = GenerateNo();// generate a number for that process
+				if (1 <= NO_Generated && NO_Generated <= 50)
+					BLK_LIST.enqueue(Run_P);
+				else
+					if (20 <= NO_Generated && NO_Generated <= 30)
+						Pr_ptr2->getItem()->AddToList(Run_P);
+					else
+						if (50 <= NO_Generated && NO_Generated <= 60)
+							TRM_LIST.InsertEnd(Run_P);
+			}
+			Pr_ptr2 = Pr_ptr2->getNext();
+		}
+		Pr_ptr2 = Processors.gethead();
+		// 4- For the process at the top of the BLK list, Generate a random number from 1 to 100. If this number is less than 10, move the process from BLK to RDY
+		NO_Generated = GenerateNo();
+		if (NO_Generated < 10)
+		{
+			BLK_LIST.dequeue(p1);
+			AddToRdy(p1);
+		}
+		// 5- For the processes in the FFS RDY list, randomly pick any process in the list and terminate it. This can be done by generating a random ID and checking if this ID is in any RDY list then kill it.
+		NO_Generated = 1 + (rand() % Processes_no);
+		for (int i = 0; i < FCFS_no; i++)
+		{
+			if (Pr_ptr3->getItem()->GetProcessById(NO_Generated, p2))
+				TRM_LIST.InsertEnd(p2);
+			Pr_ptr3 = Pr_ptr3->getNext();
+		}
+		update_TimeStep();
+	}
+}
+int scheduler::GenerateNo()
+{
+	return 1 + (rand() % 100);
+}
+void scheduler::AddToRdy(Process* p)
+{
+	if (Ctrl_Processors)
+	{
+		Ctrl_Processors->getItem()->AddToList(p);
+		Ctrl_Processors = Ctrl_Processors->getNext();
+	}
+	else
+		Ctrl_Processors = Processors.gethead();
+}
+void scheduler::update_TimeStep()
+{
+	Time_Step++;
 }
